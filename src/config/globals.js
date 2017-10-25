90b85effd13fedd71e80fa6382581fc1759711c0
KC <kcjones.2002@gmail.com>
2017-10-25 00:35:18 -0700

    Use wss with https

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,6 +1,5 @@
 'use strict';
 
 window.HOST = location.protocol+"//"+location.host;
-// TODO: Temp until we can use wss to connect
-window.WS_HOST = location.hostname === 'localhost' ? `ws://${location.host}`: `ws://${location.host}`;
+window.WS_HOST = location.protocol === 'http:' ? `ws://${location.host}` : `wss://${location.host}`;
 window.DOMAIN = location.protocol+"//"+location.hostname
